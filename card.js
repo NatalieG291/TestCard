cdd70ac599eb9097345ecab388460e472675a5c4
Soporte ossc <soporte@OSSCmexico.onmicrosoft.com>
2024-10-28 19:57:17 -0600

    correccion estado

--- card.js
+++ card.js
@@ -8,7 +8,8 @@ class ContentCardExample extends HTMLElement {
         const state = hass.states[entityId];
         const stateStr = state ? state.state : "unavailable";
 
-        const runState = state.attributes["run_state"] || state.entity_id;;
+        const runState = state.attributes["run_state"] || state.entity_id;
+        const currentCourse = state.attributes["current_course"] || state.entity_id;
         const friendlyName = state.attributes["friendly_name"] || state.entity_id;
         const temp = state.attributes["water_temp"] || state.entity_id;
         const icon = state.attributes["icon"];
@@ -105,7 +106,7 @@ class ContentCardExample extends HTMLElement {
             const remainMinutes = (parseInt(remainTime.split(":")[0]) * 60) + parseInt(remainTime.split(":")[1]);
             this.querySelector(".progress-wrapper").style.backgroundColor = "#5e467b";
             this.querySelector(".progress").style.backgroundColor = "#c290ff";
-            this.querySelector(".estado span").innerHTML = 'Ciclo actual <strong>' + state.attributes["current_course"] + '</strong> | ' + state.attributes["run_state"];
+            this.querySelector(".estado span").innerHTML = 'Ciclo actual <strong>' + currentCourse + '</strong> | ' + runState;
             if (temp == 'Sin seleccionar') {
                 this.querySelector(".temp span").innerHTML = '-';
             }
