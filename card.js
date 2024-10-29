6aff81afc0e5c58d9b84f6617108e78fbded6e1f
Soporte ossc <soporte@OSSCmexico.onmicrosoft.com>
2024-10-28 20:10:19 -0600

    contenido dinamico

--- card.js
+++ card.js
@@ -12,6 +12,10 @@ class ContentCardExample extends HTMLElement {
         const currentCourse = state.attributes["current_course"] || state.entity_id;
         const friendlyName = state.attributes["friendly_name"] || state.entity_id;
         const temp = state.attributes["water_temp"] || state.entity_id;
+        const rinse = state.attributes["rinse_mode"] || state.entity_id;
+        const spin = state.attributes["spin_speed"] || state.entity_id;
+        const pre = state.attributes["pre_wash"] || state.entity_id;
+        const dry = state.attributes["dry_level"] || state.entity_id;
         const icon = state.attributes["icon"];
         if (!this.content) {
             this.innerHTML = `
@@ -43,8 +47,8 @@ class ContentCardExample extends HTMLElement {
                                     <li style="vertical-align: middle; text-align: center;">
                                         Enjuague
                                     </li>
-                                    <li style="vertical-align: middle; text-align: center;">
-                                        <strong>${state.attributes["rinse_mode"]}</strong>
+                                    <li class="rinse" style="vertical-align: middle; text-align: center;">
+                                        <strong>${rinse}</strong>
                                     </li>
                                 </ul>
                                 <ul style="list-style: none; align-items: center; margin: 0; padding: 5px;">
@@ -54,8 +58,8 @@ class ContentCardExample extends HTMLElement {
                                     <li style="vertical-align: middle; text-align: center;">
                                         Centrifugado
                                     </li>
-                                    <li style="vertical-align: middle; text-align: center;">
-                                        <strong>${state.attributes["spin_speed"]}</strong>
+                                    <li class="spin" style="vertical-align: middle; text-align: center;">
+                                        <strong>${speed}</strong>
                                     </li>
                                 </ul>
                             </div>
@@ -67,28 +71,28 @@ class ContentCardExample extends HTMLElement {
                                     <li style="vertical-align: middle; text-align: center;">
                                         Pre-Lavado
                                     </li>
-                                    <li style="vertical-align: middle; text-align: center;">
-                                        <strong>${state.attributes["pre_wash"]}</strong>
+                                    <li class="pre" style="vertical-align: middle; text-align: center;">
+                                        <strong>${pre}</strong>
                                     </li>
                                 </ul>
                                 <ul style="list-style: none; align-items: center; margin: 0; padding: 5px;">
                                     <li style="vertical-align: middle; text-align: center;">
-                                        <ha-icon icon="mdi:weather-windy"></ha-icon>
+                                        <ha-icon icon="mdi:sun-clock"></ha-icon>
                                     </li>
                                     <li style="vertical-align: middle; text-align: center;">
                                         Secado
                                     </li>
-                                    <li style="vertical-align: middle; text-align: center;">
-                                        <strong>${state.attributes["dry_level"]}</strong>
+                                    <li class="dry" style="vertical-align: middle; text-align: center;">
+                                        <strong>${dry}</strong>
                                     </li>
                                 </ul>
                             </div>
                             <div class="progress-wrapper" style="height: 20px; width: 100%; border-radius: 10px 10px 10px 10px;">
                                 <div class="progress" style="display: inline-block; height: 20px; border-radius: 10px 10px 10px 10px;">
                                 </div>
-                                <span style="color: #FFFFFF; position: absolute; right: 33%;">50%</span>
+                                <span style="color: #FFFFFF; position: absolute; right: 33%;"></span>
                             </div>
-                            <div style="display: flex; align-items: center; justify-content: center;">
+                            <div class="remaining" style="display: flex; align-items: center; justify-content: center;">
                                 <span><strong>${state.attributes["remain_time"]}</strong> para terminar</span>
                             </div>
                         </div>
@@ -107,15 +111,50 @@ class ContentCardExample extends HTMLElement {
             this.querySelector(".progress-wrapper").style.backgroundColor = "#5e467b";
             this.querySelector(".progress").style.backgroundColor = "#c290ff";
             this.querySelector(".estado span").innerHTML = 'Ciclo actual <strong>' + currentCourse + '</strong> | ' + runState;
+            // temp water
             if (temp == 'Sin seleccionar') {
                 this.querySelector(".temp span").innerHTML = '-';
             }
+            else {
+                this.querySelector(".temp span").innerHTML = temp;
+            }
+            // rinse mode
+            if (rinse == 'Sin seleccionar') {
+                this.querySelector(".rinse span").innerHTML = '-';
+            }
+            else {
+                this.querySelector(".rinse span").innerHTML = rinse;
+            }
+            // spin speed
+            if (spin == 'Sin seleccionar') {
+                this.querySelector(".spin span").innerHTML = '-';
+            }
+            else {
+                this.querySelector(".spin span").innerHTML = spin;
+            }
+            // pre wash
+            if (pre == 'off') {
+                this.querySelector(".pre span").innerHTML = '-';
+            }
+            else {
+                this.querySelector(".pre span").innerHTML = pre;
+            }
+            // dry level
+            if (dry == 'Sin seleccionar') {
+                this.querySelector(".dry span").innerHTML = '-';
+            }
+            else {
+                this.querySelector(".dry span").innerHTML = dry;
+            }
+
             if (runState == 'Reposo') {
                 this.querySelector(".progress-wrapper span").innerHTML = 'En espera';
+                this.querySelector(".remaining span").style.display = 'none';
             }
             else {
                 this.querySelector(".progress").style.width = (totalMinutes - remainMinutes) / totalMinutes * 100 + "%";
                 this.querySelector(".progress-wrapper span").innerHTML = Math.round((totalMinutes - remainMinutes) / totalMinutes * 100) + "%";
+                this.querySelector(".remaining span").style.display = 'flex';
             }
 
             this.querySelector("ha-icon").style.color = "#c290ff";
@@ -123,6 +162,11 @@ class ContentCardExample extends HTMLElement {
         else {
             this.querySelector(".estado span").innerHTML = 'Apagado';
             this.querySelector(".temp").innerHTML = '-';
+            this.querySelector(".rinse").innerHTML = '-';
+            this.querySelector(".spin").innerHTML = '-';
+            this.querySelector(".pre").innerHTML = '-';
+            this.querySelector(".dry").innerHTML = '-';
+            this.querySelector(".remaining span").style.display = 'none';
             this.querySelector("ha-icon").style.color = "#5e467b";
         }
 
